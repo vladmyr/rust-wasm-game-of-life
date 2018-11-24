7f35ed33f0dba14a20686a425982a836d2e3290f
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-11-24 14:19:44 +0100

    js -- implemented frame rate control

--- www/index.js
+++ www/index.js
@@ -3,13 +3,26 @@ import { Universe } from "rust-wasm-game-of-life";
 const pre = document.getElementById("game-of-life-canvas");
 const universe = Universe.new();
 
+const frameRate = (cb, fps, timestampMark = 0) => {
+  const timestampDelta = Math.floor(1000 / fps);
+
+  return (timestamp) => {
+    let newTimestampMark = timestampMark;
+
+    if (timestamp >= timestampMark) {
+      cb();
+      newTimestampMark = timestamp + timestampDelta;
+    }
+
+    requestAnimationFrame(frameRate(cb, fps, newTimestampMark));
+  }
+}
+
 const renderLoop = () => {
   const textContent = universe.render();
 
   pre.textContent = textContent;
   universe.tick();
-
-  requestAnimationFrame(renderLoop);
 }
 
-requestAnimationFrame(renderLoop);
+requestAnimationFrame(frameRate(renderLoop, 2));
