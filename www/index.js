57bb553f62c59216108419f1fa0f1bdf2133caf7
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-11-24 15:03:11 +0100

    rs, js -- expose universe selectors to js; use canvas for drawing

--- www/index.js
+++ www/index.js
@@ -1,7 +1,72 @@
-import { Universe } from "rust-wasm-game-of-life";
+import { Universe, Cell } from "rust-wasm-game-of-life";
+import { memory } from "rust-wasm-game-of-life/rust_wasm_game_of_life_bg";
 
-const pre = document.getElementById("game-of-life-canvas");
+const CELL_SIZE = 12;
+const GRID_COLOR = "#cccccc";
+const DEAD_COLOR = "#ffffff";
+const ALIVE_COLOR = "#000000";
+
 const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext("2d");
+
+const renderLoop = () => {
+  universe.tick();
+  
+  drawGrid();
+  drawCells();
+}
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+}
+
+const getIndex = (row, column) => row * width + column;
+
+const drawCells = () => {
+  const cellsPtr = universe.cells();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = getIndex(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead
+        ? DEAD_COLOR
+        : ALIVE_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+}
 
 const frameRate = (cb, fps, timestampMark = 0) => {
   const timestampDelta = Math.floor(1000 / fps);
@@ -16,13 +81,6 @@ const frameRate = (cb, fps, timestampMark = 0) => {
 
     requestAnimationFrame(frameRate(cb, fps, newTimestampMark));
   }
-}
-
-const renderLoop = () => {
-  const textContent = universe.render();
-
-  pre.textContent = textContent;
-  universe.tick();
 }
 
 requestAnimationFrame(frameRate(renderLoop, 2));
