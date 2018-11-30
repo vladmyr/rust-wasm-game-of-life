cf6672637eb5c3cc53777848f5c8a7fd36562d8d
Volodymyr Khytskyi <khytsky.vladimir@gmail.com>
2018-11-30 22:06:18 +0100

    rs -- unit test

--- src/lib.rs
+++ src/lib.rs
@@ -38,10 +38,11 @@ pub enum Cell {
 #[wasm_bindgen]
 #[repr(u8)]
 pub enum UniversePreset {
-    Demo = 0,
-    Random = 1,
-    Glider = 2,
-    SpaceShip = 3,
+    Empty = 0,
+    Demo = 1,
+    Random = 2,
+    Glider = 3,
+    SpaceShip = 4,
 }
 
 #[wasm_bindgen]
@@ -133,6 +134,9 @@ impl Universe {
 
         let cells = (0..width * height).map(|i| {
             match preset {
+                UniversePreset::Empty => {
+                    Cell::Dead
+                },
                 UniversePreset::Demo => {
                     if i % 2 == 0 || i % 7 == 0 {
                         Cell::Alive
@@ -190,11 +194,34 @@ impl Universe {
         self.height
     }
 
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
     }
 
     pub fn render(&self) -> String {
         self.to_string()
+    }
+}
+
+impl Universe {
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
     }
 }
